51e88ef1671d80d5c5717119d542fdfe9e1e591b
Taller88 <iod1124@naver.com>
2022-10-20 22:51:23 +0900

    fs sync/async

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -1,9 +1,29 @@
-const http = require('http');
-const math =require('./math');
+const fs = require('fs');
 
 
-const result = math.sum(1,3);
 
-console.log(result);
+// 동기형식
 
-http.createServer();
+const syncData = fs.readFileSync('./data.txt');
+//syncData
+//<Buffer 54 68 69 73 20 69 73 20 64 61 74 61 20 74 78 74 21>
+//syncData.toString() 
+//This is data txt!
+
+console.log("sync toString "+syncData.toString());
+
+const syncDataUtf = fs.readFileSync('./data.txt', 'utf8');
+console.log("sync: "+syncDataUtf);
+
+
+// 비동기 
+
+const data = fs.readFile('./data.txt','utf8',(err, data)=>{
+    if(err){
+        console.log("error")
+        return;
+    }
+
+    console.log("success Callback");
+    console.log("data: "+data);
+})
